f5128330be92030aabc4f98b5afe69d51d9b7c7c
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-05-15 22:01:22 +0400

    init tailwind

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
-const App = () => {
-  return <div>hello</div>;
+const App = (): JSX.Element => {
+  return <h1 className="text-3xl font-bold underline">Hello sudoku!</h1>;
 };
 
 export default App;
