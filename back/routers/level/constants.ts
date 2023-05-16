5ee899deb6a40b7f62476392f130a143c9d871f6
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-05-16 12:44:47 +0400

    create level router

---
+++ back/routers/level/constants.ts
@@ -0,0 +1,19 @@
+const BASE_URL = "https://www.meme-arsenal.com/memes";
+
+export const LEVELS = [
+  {
+    id: "easy",
+    title: "Легкий уровень",
+    img: `${BASE_URL}/c2660bc7ed210868db568f833e8548fb.jpg`,
+  },
+  {
+    id: "medium",
+    title: "Средний уровень",
+    img: `${BASE_URL}/887c4565db2d97ff3a0475b78b296762.jpg`,
+  },
+  {
+    id: "hard",
+    title: "Сложный уровень",
+    img: `${BASE_URL}/887c4565db2d97ff3a0475b78b296762.jpg`,
+  },
+];
--- back/routers/level/index.ts
+++ back/routers/level/index.ts
@@ -1,9 +1,15 @@
 import { Response, Request, Router } from "express";
 
+import { LEVELS } from "./constants";
+
 const router = Router();
 
 router.get("/", async (r: Request, res: Response) => {
-  res.json({ test: "game router" });
+  try {
+    res.json({ levels: LEVELS });
+  } catch (e) {
+    res.status(500).json({ message: "Unexpected server error" });
+  }
 });
 
 export default router;
